eee2563ae7e08667b1ce0b82d7c04ead8b33a910
Alex <alexander.wow@hotmail.se>
2017-05-10 16:08:32 +0200

    Test case for user story 4.

--- Test.java
+++ Test.java
@@ -2,6 +2,8 @@ package lab5new;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 public class Test {
 
 	@org.junit.Test
@@ -21,6 +23,23 @@ public class Test {
 	public void testGame() {
 		Game testGame = new Game();
 		assertEquals(10,testGame.frameList.size());
+	}
+	
+	@org.junit.Test
+	public void testGameScore() {
+		ArrayList<Frame> testFrameList = new ArrayList<Frame>();
+		testFrameList.add(new Frame(1,5));
+		testFrameList.add(new Frame(3,6));
+		testFrameList.add(new Frame(7,2));
+		testFrameList.add(new Frame(3,6));
+		testFrameList.add(new Frame(4,4));
+		testFrameList.add(new Frame(5,3));
+		testFrameList.add(new Frame(3,3));
+		testFrameList.add(new Frame(4,5));
+		testFrameList.add(new Frame(8,1));
+		testFrameList.add(new Frame(2,6));
+		Game testGame = new Game(testFrameList);
+		assertEquals(81,testGame.frameList.size());
 	}
 	
 	
