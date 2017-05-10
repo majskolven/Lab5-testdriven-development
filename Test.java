fbf7a39b4b1acb8589099fc83ece80128a81dcaf
Alex <alexander.wow@hotmail.se>
2017-05-10 15:24:37 +0200

    Test case for user story 1.

---
+++ Test.java
@@ -0,0 +1,13 @@
+package lab5new;
+
+import static org.junit.Assert.*;
+
+public class Test {
+
+	@org.junit.Test
+	public void testFrame() {
+		Frame testFrame = new Frame(2,4);
+		assertEquals(2,testFrame.getThrow1());
+		assertEquals(4,testFrame.getThrow2());
+	}
+}
