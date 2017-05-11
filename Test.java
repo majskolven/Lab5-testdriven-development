3688ad1e07bbc891b5a22838a14ce77e2bff79b0
Alex <alexander.wow@hotmail.se>
2017-05-11 12:55:24 +0200

    Test case for user story 9.

--- Test.java
+++ Test.java
@@ -109,4 +109,21 @@ public class Test {
 		Game testGame = new Game(testFrameList);
 		assertEquals(112,testGame.getTotalScore());
 	}
+	
+	@org.junit.Test
+	public void testMultipleSpares() {
+		ArrayList<Frame> testFrameList = new ArrayList<Frame>();
+		testFrameList.add(new Frame(8,2));
+		testFrameList.add(new Frame(5,5));
+		testFrameList.add(new Frame(7,2));
+		testFrameList.add(new Frame(3,6));
+		testFrameList.add(new Frame(4,4));
+		testFrameList.add(new Frame(5,3));
+		testFrameList.add(new Frame(3,3));
+		testFrameList.add(new Frame(4,5));
+		testFrameList.add(new Frame(8,1));
+		testFrameList.add(new Frame(2,6));
+		Game testGame = new Game(testFrameList);
+		assertEquals(98,testGame.getTotalScore());
+	}
 }
